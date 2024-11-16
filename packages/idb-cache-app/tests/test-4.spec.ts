08070c36113f4715039cf2679cb25d9a44b2e03c
Aaron Shafovaloff <ashafovaloff@instructure.com>
2024-11-16 13:57:14 -0700

    Add test: 20mb size item

---
+++ packages/idb-cache-app/tests/test-4.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from "@playwright/test";
+
+test("20mb size item", async ({ page }) => {
+  await page.goto("http://localhost:3000/#size=20480");
+  await page.getByTestId("clear-button").click();
+  await page.getByTestId("set-item-button").click();
+  await expect(page.getByText("6u81xr")).toBeVisible();
+  await page.getByTestId("get-item-button").click();
+  await expect(page.getByTestId("hash2").getByText("6u81xr")).toBeVisible();
+  await page.getByTestId("count-button").click();
+  await expect(page.getByText("839")).toBeVisible();
+});
