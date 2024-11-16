9266b327c43c0fcac67e4096039de321d1e5e7f2
Aaron Shafovaloff <ashafovaloff@instructure.com>
2024-11-16 13:46:00 -0700

    Tests against Firefox

--- packages/idb-cache-app/playwright.config.ts
+++ packages/idb-cache-app/playwright.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="node" />
 import { defineConfig, devices } from "@playwright/test";
 
 /**
@@ -36,18 +37,16 @@ export default defineConfig({
   projects: [
     {
       name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      use: { ...devices["Desktop Chrome"], headless: true },
     },
-
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
-
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"], headless: true },
+    },
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"], headless: true },
+    },
 
     /* Test against mobile viewports. */
     // {
