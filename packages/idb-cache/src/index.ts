0f706895bcb0e857500a06f10f83732f511d5af9
Aaron Shafovaloff <ashafovaloff@instructure.com>
2024-11-25 20:42:39 -0700

    docs: add a few comments

--- packages/idb-cache/src/index.ts
+++ packages/idb-cache/src/index.ts
@@ -94,9 +94,9 @@ export interface IDBCacheConfig {
 
 const DB_VERSION = 2;
 const DEFAULT_CHUNK_SIZE = 25000; // recommendation: keep under 100KiB (cf. https://surma.dev/things/is-postmessage-slow/)
-const DEFAULT_GC_TIME = 7 * 24 * 60 * 60 * 1000;
+const DEFAULT_GC_TIME = 7 * 24 * 60 * 60 * 1000; // 7 days
 const DEFAULT_PBKDF2_ITERATIONS = 100000;
-const CLEANUP_INTERVAL = 60 * 1000;
+const CLEANUP_INTERVAL = 60 * 1000; // 1 minute
 const DURATION_THRESHOLD = 200;
 
 const isSubtleCryptoSupported = crypto?.subtle;
